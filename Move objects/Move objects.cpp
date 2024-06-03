669b23aee2d891e6d1e8f3de2a90cdf6739fddda
Knyazev Nikita <nk8222138@gmail.com>
2024-06-04 00:26:23 +0300

    first commit

---
+++ Move objects/Move objects.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+#include <vector>
+
+template <typename T>
+
+void move_vectors(T& a, T& b)
+{
+  b = std::move(a);
+}
+
+int main()
+{
+   std::vector <std::string> one = { "test_string1", "test_string2" };
+   std::vector <std::string> two;
+   move_vectors(one, two);
+
+   for (const auto & c : two)
+   {
+      std::cout << c <<" ";
+   }
+   std::cout << std::endl;
+
+   return 0;
+}
+
